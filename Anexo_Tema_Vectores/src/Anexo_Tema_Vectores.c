de8529f7e56f85f37163d15204f26efad790986e
villalbapaezmiguel <villalbapaezmiguel@gmail.com>
2022-04-12 22:31:22 -0300

    clase 6 de laboratoio

--- Anexo_Tema_Vectores/src/Anexo_Tema_Vectores.c
+++ Anexo_Tema_Vectores/src/Anexo_Tema_Vectores.c
@@ -8,8 +8,9 @@
  ============================================================================
  */
 
-#include <stdio.h>
+#include <stdio_ext.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
 /*ANEXO 5-1:
@@ -19,8 +20,42 @@ Armar un Menú de Opciones con las siguientes opciones
 3-Mostrar
 4-Calcular Promedio
 5-Ordenar
-Al ingresar a cada opción del menú deberá imprimir en pantalla el nombre del mismo. Ej: Si se presiona la opción 1 mostrar por pantalla “Ud. ha seleccionado lo opción 1-Inicializar”
+Al ingresar a cada opción del menú deberá imprimir en pantalla el nombre del mismo.
+Ej: Si se presiona la opción 1 mostrar por pantalla “Ud. ha seleccionado lo opción 1-Inicializar”
 */
+	setbuf(stdout, NULL);
+	int opcion ;
+	char caracter ;
+
+	do {
+		printf("\n1-Inicializar \n2-Cargar \n3-Mostrar \n4-Calcular Promedio \n5-Ordenar");
+		printf("\nIngrese una opcion : ");
+		scanf("%d", &opcion);
+
+		switch(opcion)
+		{
+			case 1:
+				printf("\nInicializar");
+				break;
+			case 2:
+				printf("\nCargar");
+				break;
+			case 3:
+				printf("\nMostrar");
+				break;
+			case 4:
+				printf("\nCalcular Promedio");
+				break;
+			case 5:
+				printf("\nOrdenar");
+				break;
+			default : printf("\nOpcion no valida ");
+		}
+		printf("\ndesea continuar (s / n) ? : ");
+		__fpurge(stdin);
+		scanf("%c", &caracter);
+
+	} while (caracter == 's');
 
 	return EXIT_SUCCESS;
 }
--- CrearUnMenu/src/CrearUnMenu.c
+++ CrearUnMenu/src/CrearUnMenu.c
@@ -16,11 +16,61 @@
 int main(void) {
 	setbuf(stdout, NULL);
 
-	char cadena [15] = "programacion";
+//	char cadena [15] = "programacion";
+//
+//	printf("\nINgrese algo : ");
+//	__fpurge(stdin);//lo estas limpiando
+//	scanf("%s", cadena);
+//
+//	char nombre[31];
+//
+//	printf("\nIngrese nombre : ");
+//	__fpurge(stdin);
+////	scanf("%s", nombre);
+//
+//	printf("\nIngreso : %s", nombre);
 
-	printf("\nINgrese algo : ");
-	__fpurge(stdin);//lo estas limpiando
-	scanf("%s", cadena);
+/*Ejercicio 6-3:
+Pedirle al usuario su nombre y apellido (en variables separadas,
+una para el nombre y otra para el apellido). Ninguna de las dos variables se puede modificar.
+Debemos lograr guardar en una tercer variable el apellido y el nombre con el siguiente formato:
+Por ejemplo:
+Si el nombre es juan ignacio y el apellido es gOmEz, la salida debería ser:
+Gomez, Juan Ignacio*/
+
+	char nombre [31];
+	char apellido [31];
+	char nombre_apellido[71];
+	int cantidad  = 0;
+	printf("\nIngrese nombre :");
+	__fpurge(stdin);
+	scanf("%[^\n]",nombre);
+
+	printf("\nIngrese apellido :");
+	__fpurge(stdin);
+	scanf("%[^\n]",apellido);
+
+	//paso a minuscula las cadenas
+	strlwr(apellido);
+	strlwr(nombre);
+
+	//paso las iniciales a mayusculas
+	apellido[0] = toupper(apellido[0]);
+
+	nombre[0] = toupper(nombre[0]);
+	cantidad = strlen(nombre);
+	for (int i = 0; i < cantidad ; ++i) {
+
+		if(nombre[i] == ' ')
+		{
+			nombre[i+1] = toupper(nombre[i+1]);
+
+		}
+	}
+
+	nombre_apellido[0] = '\0';//limpiamos la mememria que nos resrvo : porque cuando le pedimos un espacio de memerio , nos lo van a dar , pero va  estar con basura
+
+	//concateno apellido y nombre en una variable
 
 
 
